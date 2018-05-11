3fdffc9c0d1e606162d7858f174b0dd1a4dd09fc
zephyr-protocol <jonjonsnow11@gmail.com>
2018-05-11 23:18:06 +0300

    Zephyr GUI Wallet

---
+++ settings/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+## Copyright (c) 2017-2018, The Sumokoin Project (www.sumokoin.org)
+'''
+App top-level settings
+'''
+
+__doc__ = 'default application wide settings'
+
+import sys
+import os
+import logging
+
+from utils.common import getHomeDir, makeDir
+
+USER_AGENT = "Zephyr GUI Wallet"
+APP_NAME = "Zephyr GUI Wallet"
+VERSION = [1, 1, 0]
+
+
+_data_dir = makeDir(os.path.join(getHomeDir(), 'ZephyrGUIWallet'))
+DATA_DIR = _data_dir
+
+log_file  = os.path.join(DATA_DIR, 'logs', 'app.log') # default logging file
+log_level = logging.DEBUG # logging level
+
+seed_languages = [("0", "English"), 
+                  ("1", "Spanish"), 
+                  ("2", "German"), 
+                  ("3", "Italian"), 
+                  ("4", "Portuguese"),
+                  ("5", "Russian"),
+                  ("6", "Japanese"),
+                ]
+
+# COIN - number of smallest units in one coin
+COIN = 1000000000.0
